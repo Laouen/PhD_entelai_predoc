a663f5c83fd82c603bf5353d68f2e9b4962d700a
Laouen <laouen.belloli@gmail.com>
2021-08-09 22:20:30 +0000

    implemented preprocess data

--- entelai_predoc_modules/preprocess_data.py
+++ entelai_predoc_modules/preprocess_data.py
@@ -1,116 +1,7 @@
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer, MinMaxScaler
 import pandas as pd
+import numpy as np
 import json
-
-
-def json_to_csv(json_data):
-    """
-    Converts json responses to dataframe format
-    """
-    df = pd.DataFrame()
-    for elem in json_data:
-        df = pd.concat([df, pd.DataFrame(elem)], axis=1, sort=False)
-    df = df.transpose()
-    return df
-
-
-def filter_diagnostics(df):
-    error_col = 'En cuanto a la intensidad del dolor, señale en una escala de 1 a 10 la intensidad máxima a la que han llegado sus dolores'
-    # Remove nan in condition
-    new_df = df.copy()
-    #remove all test ids
-    new_df = new_df[new_df['practitioner_id'] != 15]
-    new_df.dropna(subset=['condition'], inplace=True)
-    # Format condition values
-    new_df.condition = new_df.condition.apply(
-        lambda x: x.replace('\n', ' ').replace('  ', ' ').strip())
-
-    # Unify diagnostics using diagnostic_dict
-    new_df.condition.replace(diagnostics_dict, inplace=True)
-    # Remove None condition again
-    new_df.dropna(subset=['condition'], inplace=True)
-
-    error_col_values = new_df[error_col].apply(extract_value).astype(float)
-    print(error_col_values)
-    # Get index of inconsisten rows
-    rows_to_drop = error_col_values[error_col_values > 10.0].index
-
-    new_df.drop(rows_to_drop, inplace=True)
-
-    print(new_df.condition.value_counts())
-    data = new_df.drop(columns=['ai_condition', 'ai_eval', 'medical_consultation_id',
-                                'condition', 'practitioner_id', 'questionnarie_id'])
-    target = new_df['condition']
-    target[target.isin(['Cefalea secundaria',
-                        'Cefalea atribuída a disfunción temporomandibular',
-                        'Cefalea por abuso de medicación', 'Neuralgia del intermediario',
-                        'Cefalea asociada al ejercicio', 'Síndroma de boca ardiente'])] = 'Otras'
-    return data, target
-
-
-def extract_value(x):
-    try:
-        if len(x) == 1:
-            return x[0]
-        return x.replace("['", "").replace("']", "")
-    except:
-        return x
-
-
-def one_hot_encode_multi_data(data):
-    # Replace list values
-    multiple_value_cols = set()
-    for label, content in data.items():
-        try:
-            if any(map(lambda x: (type(x) == list and len(x) > 1), content)):
-                multiple_value_cols.add(label)
-        except:
-            continue
-    multiple_cols_data = data[multiple_value_cols]
-
-    # OneHotEncode over multiple_values
-    res_multiple_value_data = pd.DataFrame()
-    for c in multiple_value_cols:
-        
-        partial_df = multiple_cols_data[c].str.join(sep='*').str.get_dummies(sep='*')
-        
-        partial_df.rename(columns=dict(
-            map(lambda x: (x, '{}_{}'.format(c, x)), partial_df.columns)), inplace=True)
-
-        res_multiple_value_data = pd.concat(
-            [res_multiple_value_data, partial_df], axis=1, sort=False)
-    return res_multiple_value_data
-
-
-def one_hot_encode_cat_data(cat_data):
-    cat_data = cat_data.applymap(extract_value)
-    return pd.get_dummies(cat_data)
-
-
-def preprocess_training(data):
-    cat_dummies = one_hot_encode_cat_data(data[object_features_names])
-    multi_dummies = one_hot_encode_multi_data(data[multi_features_names])
-    num = data[num_features_names].applymap(extract_value).astype(float)
-    return cat_dummies.join(multi_dummies).join(num)  # [boruta_cols]
-
-
-def preprocess_instance(instance):
-    # Create empty df with boruta columns
-
-    instance_df = pd.DataFrame(columns=boruta_cols)
-    instance_cat = pd.DataFrame(columns=object_features_names)
-    instance_cat = one_hot_encode_cat_data(instance_cat.append(
-        instance)[object_features_names].applymap(extract_value)).dropna(axis=1)
-
-    instance_num = instance[num_features_names].applymap(
-        extract_value).astype(float).astype(int)
-
-    instance_mul = pd.DataFrame(columns=multi_features_names)
-    instance_mul = one_hot_encode_multi_data(instance_mul.append(instance)[
-                                             multi_features_names]).dropna(axis=1)
-
-    return instance_df.append(instance_cat.join(instance_mul).join(instance_num))[boruta_cols].fillna(0).values
 
 
 subject_info_cols = [
@@ -213,48 +104,53 @@ columns_to_drop = [
 
 # Convert values to arrays and clean values
 def extract_value(x):
-    if x.startswith('['):
-        return [item.strip().replace(' ','_') for item in json.loads(x.replace("'",'"'))]
-    elif x == '':
-        return []
-    elif type(x) == str:
-        return [x.strip().replace(' ','_')]
+    if type(x) == str:
+        if x.startswith('['):
+            try:
+                return [
+                    item.strip().replace(' ', '_')
+                    for item in json.loads(x.replace('"','').replace("'",'"'))
+                ]
+            except Exception as e:
+                print(x)
+                raise e 
+        elif x == '':
+            return []
+        else:
+            return [x.strip().replace(' ','_')]
     else:
         return [x]
 
-def transform_column_values(col):
-    return col.apply(extract_value)
 
 def extract_numerical_value(x):
-    int(float(x.replace('[','').replace(']',''))))
+    if type(x) == str:
+        return int(float(x.replace('[','').replace(']','').replace("'","").replace('"','')))
+    else:
+        return x
 
-def preprocess_data(curated_curated, data_entelai_predoc_server):
+
+def preprocess_data(curated_targets, data_entelai_predoc_server):
     
     # Read curated and server data and merge them 
-    df_curated_targets = pd.read_excel(
-        curated_curated,
-        columns=['Diagnóstico del médico', *subject_info_cols]
-    )
+    df_curated_targets = pd.read_excel(curated_targets)
+    df_curated_targets = df_curated_targets[subject_info_cols_rename.keys()]
 
     df_curated_targets.rename(
         subject_info_cols_rename,
-        inplace=True
+        inplace=True,
+        axis=1
     )
 
     df_predoc_responses = pd.read_csv(
         data_entelai_predoc_server,
+        sep=';',
         keep_default_na=False
     ).drop(columns_to_drop, axis=1)
 
-    # Remove subject with incorrect data in some cols
-    df_predoc_responses = df_predoc_responses[df_predoc_responses.patient_identifier == 17560351]
+    df_predoc_responses['questionnarie_date'] = pd.to_datetime(df_predoc_responses['questionnarie_date']).dt.date.astype('datetime64[ns]')
 
-    # Me quedo con la primer visita de cada paciente para evitar diagnosticos sesgados
-    df_predoc_responses.drop_duplicates(
-        subset='patient_identifier',
-        keep='first',
-        inplace=True
-    )
+    # Remove subject with incorrect data in some cols
+    df_predoc_responses = df_predoc_responses[df_predoc_responses.patient_identifier != 17560351]
 
     # Mergeo el dataframe con el target con el dataframe con los features
     df = pd.merge(
@@ -263,14 +159,37 @@ def preprocess_data(curated_curated, data_entelai_predoc_server):
         on=subject_info_cols,
         how='inner',
         validate='1:1'
-    ).drop(subject_info_cols, axis=1)
+    )
+
+    # Me quedo con la primer visita de cada paciente para evitar diagnosticos sesgados
+    df.drop_duplicates(
+        subset='patient_identifier',
+        keep='first',
+        inplace=True
+    )
+
+    # Elimino las columnas que son info del paciente
+    df.drop(
+        subject_info_cols,
+        axis=1,
+        inplace=True
+    )
+
+    df = df.reset_index(drop=True)
 
     # clean column names
-    df.rename(lambda x: x.strip().replace(' ','_'), inplace=True)
+    df.rename(
+        lambda x: x.strip().replace(' ','_'),
+        inplace=True,
+        axis=1
+    )
 
-    # process numerical cols
-    for col in numerical_features:
-        df[col] = df[col].apply(extract_numerical_value)
+    numerical_features = [c.strip().replace(' ','_') for c in numerical_features]
+
+    df[numerical_features] = df[numerical_features].apply(
+        lambda col: col.apply(extract_numerical_value),
+        axis=1
+    )
 
     # scale numerical features between 0 and 1
     scaler = MinMaxScaler()
@@ -283,17 +202,27 @@ def preprocess_data(curated_curated, data_entelai_predoc_server):
     ]
 
     # One hot encode categorical features
-    df[array_columns] = df[array_columns].apply(transform_column_values, axis=1)
+    df[array_columns] = df[array_columns].apply(
+        lambda col: col.apply(extract_value), 
+        axis=1
+    )
 
     # Binarize multi labels
+    new_cols = []
     for col in array_columns:
         mlb = MultiLabelBinarizer()
-        new_cols = mlb.fit_transform(df[col].values)
-        df[[f'{col}_{c}' for c in mlb.classes_]] = new_cols
+        X = mlb.fit_transform(df[col].values)
+        new_cols.append([f'{col}_{c}' for c in mlb.classes_])
+        df[new_cols[-1]] = X
         df.drop(col, axis=1, inplace=True)
 
-    # extract final X, y values
-    X = df.drop('target', axis=1).values
-    y = df['target'].values
+    feature_cols = np.concatenate(new_cols + [numerical_features])
 
-    return {'X': X, 'y': y}
+    # Extract final X
+    X = df[feature_cols]
+
+    # Encode y label
+    y = df['target'].values
+    y = LabelEncoder().fit_transform(y)
+
+    return {'X': X, 'y': y, 'features': feature_cols}
